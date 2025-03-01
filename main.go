801ea63fe5f59494a02139316a4df021aed44881
Matvey Fedorov <mfgolden@yandex.ru>
2025-03-01 12:22:27 +0300

    combining previous changes together in complete programm

--- main.go
+++ main.go
@@ -1,41 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	"github.com/MatTwix/Go-online-resume-module/api"
 	"github.com/MatTwix/Go-online-resume-module/config"
+	"github.com/MatTwix/Go-online-resume-module/routes"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
+	app := fiber.New()
 	cfg := config.LoadConfig()
 
-	githubUser, err := api.GetGithubUser(cfg.GithubName)
-	if err != nil {
-		fmt.Println("Ошибка получения данных GitHub:", err)
-		return
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{cfg.AppUrl + ":" + cfg.ReactPort},
+		AllowMethods: []string{"GET", "PUT"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
+
+	routes.SetupRoutes(app)
+
+	if err := app.Listen(":" + cfg.Port); err != nil {
+		log.Fatal("Error starting server: ", err)
 	}
-
-	vkUser, err := api.GetVkUser(cfg.VkToken, cfg.VkUserID)
-	if err != nil {
-		fmt.Println("Ошибка получения данных VK:", err)
-		return
-	}
-
-	codeforcesUser, err := api.GetCodeforcesUser(cfg.CodeforcesHandle)
-	if err != nil {
-		fmt.Println("Ошибка получения данных Codeforces:", err)
-		return
-	}
-
-	data := fmt.Sprintf("Github: %s\nVK: %s\nCodeforces: %s", githubUser, vkUser, codeforcesUser)
-
-	err = os.WriteFile("output/data.txt", []byte(data), 0644)
-	if err != nil {
-		fmt.Println("Ошибка записи файла:", err)
-		return
-	}
-
-	fmt.Println("Данные сохранены в output/data.txt")
 }
